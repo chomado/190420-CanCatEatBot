95a56366d299dc24e76a14459b632c6a435884e9
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2019-04-17 00:23:33 +0900

    intent 振り分け

--- Controllers/ClovaController.cs
+++ Controllers/ClovaController.cs
@@ -20,6 +20,52 @@ namespace CoreBot.Controllers
         [HttpPost]
         public async Task<IActionResult> Post()
         {
+            CEKRequest request = await client.GetRequest(
+                Request.Headers["SignatureCEK"],
+                Request.Body
+            );
+
+            var response = new CEKResponse();
+
+            switch (request.Request.Type)
+            {
+                case RequestType.LaunchRequest:
+                    {
+                        response.AddText("こんにちは！これ食べれるニャンボットです。人間の食べ物を入力すると、これは猫が食べて大丈夫かどうかを答えるよ");
+                        response.ShouldEndSession = false;
+                        break;
+                    }
+                case RequestType.SessionEndedRequest:
+                    {
+                        response.AddText("またね～");
+                        break;
+                    }
+                case RequestType.IntentRequest:
+                    {
+                        switch (request.Request.Intent.Name)
+                        {
+                            case "CatIntent": // "パン"
+                                {
+                                    // 食べれるものが渡ってくるので何か処理をする TODO!
+                                    response.ShouldEndSession = false;
+                                    break;
+                                }
+                            case "ChomadoIntent": // "ちょまど"
+                                {
+                                    response.AddText("ちょまどさん、さすがオトナです！素敵！すばらしい！松屋！");
+                                    response.ShouldEndSession = false;
+                                    break;
+                                }
+                            case "Clova.GuideIntent":
+                                {
+                                    response.AddText("パン、と言ってみてください。");
+                                    response.ShouldEndSession = false;
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+            }
             return new OkObjectResult(true);
         }
 
