fcd2efdd9bae6bade2b48c18c7d439f548b3fc83
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2019-04-16 23:15:53 +0900

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=190420-wikibot-Web-Deploy.pubxml -p:Password=EQZQCbFzpkQH23Pgx0EqgsaYWquhBmtbWqHs4c30RuwSKwP3LntHCirdWBku
+
